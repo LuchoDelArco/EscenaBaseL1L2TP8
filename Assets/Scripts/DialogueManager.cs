c8368436306b913a6ca1513fabf0380d33b12bbe
LuchoDelArco <47138072@est.ort.edu.ar>
2022-09-10 17:48:05 -0300

    InteractionFinished

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -5,12 +5,18 @@ using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
-	[SerializeField] string[] arrayMisiones;
+	[SerializeField] string[] arrayDialogos;
+
 	[SerializeField] GameObject interactionText;
+
 	[SerializeField] GameObject panelDialogos;
+	[SerializeField] TextMeshProUGUI textoDelDialogo;
+
+	[SerializeField] int posicionFrase;
 
 	bool interactionActive;
 	bool startedDialogue;
+	bool talkFinished;
 
 
     // Start is called before the first frame update
@@ -18,25 +24,74 @@ public class DialogueManager : MonoBehaviour
     {
 		interactionText.SetActive(false);
 		panelDialogos.SetActive(false);
+
+		posicionFrase = -1;
 	}
 
     // Update is called once per frame
     void Update()
     {
-        if(interactionActive && Input.GetKeyDown(KeyCode.R))	//Si se activó la interacción y se presiona R
+		if(interactionActive && Input.GetKeyDown(KeyCode.R) && !startedDialogue)    //Si se activó la interacción, se presiona R y es la primer interaccion
 		{
-			EmpezarDialogo();	//Se invoca...
+			EmpezarDialogo();   //Se invoca...		
 		}
+		NextFrase();
     }
 
-	void OnTriggerEnter(Collider other)
+	
+
+	void EmpezarDialogo()
 	{
+		startedDialogue = true;     //Empezó el dialogo
+		panelDialogos.SetActive(true);      //Activo el panel para el texto
+		interactionText.SetActive(false);   //Desactivo el mensaje para interactuar
+	}
+
+	void NextFrase()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			posicionFrase++;        //Aumenta el indice del dialogo
+
+			if (posicionFrase < arrayDialogos.Length) //Si todavía no se llego al final de los dialogos...
+			{
+				textoDelDialogo.text = arrayDialogos[posicionFrase];    //El texto que se muestra es el de la posición del indice
+
+			}
+			else
+			{
+				panelDialogos.SetActive(false);
+				talkFinished = true;
+				//interactionText.SetActive(true);
+			}
+		}
+	}
+
+
+	void OnTriggerEnter(Collider other)		//Al entrar en contacto
+	{	
 		if (other.gameObject.CompareTag("NPC"))
 		{
-			interactionText.SetActive(true);
+			arrayDialogos = other.gameObject.GetComponent<NpcBehaviour>().data.dialogueFrases;
 
-			interactionActive = true;
+			if (!startedDialogue)  //Si no habló todavía...
+			{
+				interactionText.SetActive(true);    //Se activa la opcion de interaccion
+				interactionActive = true;
+
+			}
+			else if (startedDialogue && posicionFrase == arrayDialogos.Length)	//Si ya hablo previamente...
+			{
+				textoDelDialogo.text = "ya hemos hablado";
+				panelDialogos.SetActive(true);		
+			}	
+			else //Si estaba hablando
+			{
+				panelDialogos.SetActive(true);
+			} 
+			
 		}
+
 	}
 
 	void OnTriggerExit(Collider other)
@@ -44,13 +99,18 @@ public class DialogueManager : MonoBehaviour
 		if (other.gameObject.CompareTag("NPC"))
 		{
 			interactionText.SetActive(false);
+			panelDialogos.SetActive(false);
 		}
 	}
 
-	void EmpezarDialogo()
-	{
-		startedDialogue = true;		//Empezó el dialogo
-		panelDialogos.SetActive(true);		//Activo el panel para el texto
-		interactionText.SetActive(false);	//Desactivo el mensaje para interactuar
-	}
+	
+
+
+	//public void NextFrase()
+	//{
+		
+
+	//}
+
+
 }
---
+++ Assets/Scripts/NpcBehaviour.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcBehaviour : MonoBehaviour
+{
+   public NPCData data;
+}

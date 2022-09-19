8d91b10912924051059149703271e4e80bb51907
LuchoDelArco <47138072@est.ort.edu.ar>
2022-09-19 12:04:31 -0300

    ClassFinished

---
+++ Assets/Scripts/Tp9/CameraManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+
+	public GameObject camIntro;
+	public GameObject camPlayer;
+	
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		camPlayer.SetActive(false);
+		camIntro.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void ActivarCamara()
+	{
+		camPlayer.SetActive(true);
+		camIntro.SetActive(false);
+	}
+}
---
+++ Assets/Scripts/Tp9/CmeraScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CmeraScript : MonoBehaviour
+{
+
+	public CameraManager camManager;
+
+
+	public void ActivateCamera()
+	{
+		camManager.ActivarCamara();
+	}
+}
